2be1e45658145b0a45540f79e28488d38b25076a
notEvanN <vudutminh@gmail.com>
2025-07-07 02:50:16 +1000

    2.0: customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+import { Divide } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-      <Button className="text-white">
-        <input type="text" />
-      </Button>
+    <div className="p-4">
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <Button variant="elevated">
+          I am button
+        </Button>
+      </div>
+      <div>
+        <Input placeholder="I am input"/>
+      </div>
+      <div>
+        <Progress value={50}/>
+      </div>
+      <div>
+        <Textarea placeholder="I am textarea"/>
+      </div>
+      <div>
+        <Checkbox />
+      </div>
+    </div>
     </div>
   );
 };
